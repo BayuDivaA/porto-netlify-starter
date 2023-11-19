0bb6a4f14a3ff8c1aeb007ecff1109da47d07d2c
byudiva.a <bayuawi9@gmail.com>
2023-11-19 14:09:15 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -11,13 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Under Construction." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          You can see my other projects. <link rel="stylesheet" href="https://cryptocharity.biz.id" />
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
